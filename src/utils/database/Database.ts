d3ef8ff45e3169c919b3c79b5fa4712f2c06a401
Hieu Duong <hieuduong21092002@outlook.com>
2023-11-24 01:35:49 +0700

    Remake the database system for multi guild control..
    - Create the guild data defining all the info the database containing relating to a guild.
    - Change the save method call to each JsonLoader-derived class.

--- src/utils/database/Database.ts
+++ src/utils/database/Database.ts
@@ -1,67 +1,75 @@
-import { BannedWordList, Ranking } from "./List/WordList";
-import { JsonLoader } from "./JsonLoader";
+import { GuildData } from "./GuildData";
+import fs from "fs";
+import path from "node:path";
 
+/**
+ * The database class.
+ * Contains and manages the list of guild data.
+ * Access the guild data contained by using the guild id as the key to access the data.
+ */
 class Database {
-  private static list: {
-    [key: string]: JsonLoader;
-  } = {};
-  private saveCooldown?: NodeJS.Timeout;
+  private static guildList: {
+    [guild_id: string]: GuildData;
+  };
+  private static readonly path = path.join(".", "database");
 
   constructor() {
-    if (!Database.list.word) {
-      Database.list.word = new BannedWordList();
+    if (!(Database.guildList instanceof Object)) {
+      Database.guildList = {};
+      if (fs.existsSync(Database.path)) {
+        const guildList = fs
+          .readdirSync(Database.path)
+          .filter((name) =>
+            fs.lstatSync(path.join(Database.path, name)).isDirectory()
+          );
+
+        guildList.forEach((guildId) => {
+          Database.guildList[guildId] = new GuildData(guildId, Database.path);
+        });
+      }
     }
   }
 
-  get bannedWords() {
-    const wordList = Database.list.word as BannedWordList;
-    return wordList.bannedWords;
-  }
+  /**
+   * Add the guild with the given id to the database.
+   * @param guildId The id to add.
+   * @returns The id if success else undefined.
+   */
+  addGuild(guildId: string) {
+    if (Database.guildList[guildId] == undefined) {
+      fs.mkdirSync(path.join(Database.path, guildId));
+      Database.guildList[guildId] = new GuildData(guildId, Database.path);
 
-  add(word: string) {
-    const wordList = Database.list.word as BannedWordList;
-    const result = wordList.add(word.toLowerCase());
-    if (result) this.save();
-    return result;
-  }
-
-  wordList() {
-    const wordList = Database.list.word as BannedWordList;
-    return wordList.wordList();
-  }
-
-  remove(word: string) {
-    const wordList = Database.list.word as BannedWordList;
-    const result = wordList.remove(word.toLowerCase());
-    if (result) this.save();
-    return result;
-  }
-
-  ranking(word?: string, userId?: string): Ranking[] {
-    const wordList = Database.list.word as BannedWordList;
-    const result = wordList.ranking(word?.toLowerCase(), userId);
-
-    return result;
-  }
-
-  count(word: string, userId: string) {
-    const wordList = Database.list.word as BannedWordList;
-    wordList.count(word.trim().toLowerCase(), userId);
-
-    this.save();
-  }
-
-  save() {
-    if (this.saveCooldown) {
-      clearTimeout(this.saveCooldown);
+      return guildId;
     }
-    this.saveCooldown = setTimeout(() => {
-      Object.values(Database.list).forEach((loader) => {
-        loader.save();
-        console.log("Saved!");
-      });
-      this.saveCooldown = undefined;
-    }, 10000);
+    return undefined;
+  }
+
+  /**
+   * Get the guild data from the id.
+   * @param guildId The id to get data.
+   * @returns The data of the guild if database contains else undefined.
+   */
+  getGuildData(guildId: string) {
+    if (Database.guildList[guildId] !== undefined) {
+      return Database.guildList[guildId];
+    }
+    return undefined;
+  }
+
+  /**
+   * Remove the guild with the given id from the database.
+   * @param guildId The id to remove.
+   * @returns The id if success else undefined.
+   */
+  removeGuild(guildId: string) {
+    if (Database.guildList[guildId] !== undefined) {
+      fs.rmSync(path.join(Database.path, guildId));
+      delete Database.guildList[guildId];
+
+      return guildId;
+    }
+    return undefined;
   }
 }
 
--- src/utils/database/JsonLoader.ts
+++ src/utils/database/JsonLoader.ts
@@ -1,31 +1,47 @@
 import fs from "fs";
 import path from "node:path";
 
+/**
+ * The base class of data storing.
+ * Contains the base data storing properties and the save and load json functions.
+ */
 abstract class JsonLoader {
   abstract list: any[];
   abstract path: string;
+  abstract readonly SaveName: string;
+  private saveCooldown?: NodeJS.Timeout;
 
   /**
    * Save the data to Json file.
    */
   save() {
-    if (!fs.existsSync(path.dirname(this.path))) {
-      fs.mkdirSync(path.dirname(this.path));
+    if (this.saveCooldown) {
+      clearTimeout(this.saveCooldown);
     }
-    fs.writeFile(this.path, JSON.stringify(this.list), function (err) {
-      if (err) throw err;
-    });
+    this.saveCooldown = setTimeout(() => {
+      if (!fs.existsSync(this.path)) {
+        fs.mkdirSync(this.path);
+      }
+      fs.writeFileSync(
+        path.join(this.path, this.SaveName),
+        JSON.stringify(this.list)
+      );
+      console.log(`[DTB]: Save ${this.SaveName} to ${this.path}}`);
+
+      this.saveCooldown = undefined;
+    }, 10000);
   }
 
   /**
    * Load the data from Json file.
    */
   load() {
-    if (fs.existsSync(this.path)) {
-      fs.readFile(this.path, (err, data) => {
-        if (err) throw err;
-        this.list = JSON.parse(data.toString());
-      });
+    if (fs.existsSync(path.join(this.path, this.SaveName))) {
+      const data = fs
+        .readFileSync(path.join(this.path, this.SaveName))
+        .toString();
+      this.list = JSON.parse(data);
+      console.log(`[DTB]: Read ${this.SaveName} from ${this.path}}`);
     }
   }
 }

1d9fb62168bb4dba0dc6186e92c080fc4e9e370d
Hieu Duong <hieuduong21092002@outlook.com>
2023-11-27 01:36:47 +0700

    Change some property view in the database.
    - Change some property in the database.
    - Update the readme and environment file to remove some unused variables.
    - Create eula resolver function to check the validity of eula file to run server.

--- src/utils/mc-server/Server.ts
+++ src/utils/mc-server/Server.ts
@@ -51,7 +51,7 @@ interface PlayerInfo {
  * Minecraft server class used to control the minecraft server.
  */
 class DefaultServer implements Server {
-  private readonly directory: string;
+  protected readonly directory: string;
   private readonly arguments: Array<string>;
   private readonly timeoutMin: number;
 
@@ -71,6 +71,7 @@ class DefaultServer implements Server {
     this.timeoutMin = process.env.MC_TIMEOUT
       ? parseFloat(process.env.MC_TIMEOUT)
       : 6;
+    this.eulaResolver();
 
     this.process = undefined;
     this.starting = false;
@@ -180,6 +181,20 @@ class DefaultServer implements Server {
       result.push("-nogui");
     }
     return result;
+  }
+
+  /**
+   * Create or change the Eula file so the server can run.
+   */
+  private eulaResolver(): void {
+    const eulaPath = path.join(this.directory, "eula.txt");
+    if (fs.existsSync(eulaPath)) {
+      const data = fs.readFileSync(eulaPath).toString();
+      if (data.includes("eula=true")) {
+        return;
+      }
+    }
+    fs.writeFileSync(eulaPath, "eula=true");
   }
 
   /**
